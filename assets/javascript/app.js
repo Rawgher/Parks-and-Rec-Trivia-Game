62227387430154a72745a9cabae21c3372558fd4
Roger <rdpender3@gmail.com>
2018-07-02 23:35:49 -0700

    added pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// figure out a theme 
+// need to make arrays of questions with right and wrong answers
+// link a gif to them
+// show a timer
+// make it tell the right answer
+// display gif
+// loading screen for 3-5 seconds next question
+// make it show something when time out
+// game over
+// put a message
+// right wrong unanswered
+// play again - does not reload page just resets
