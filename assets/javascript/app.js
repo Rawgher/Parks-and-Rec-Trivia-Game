dd2499bdf81dcb3b3cfc5eb114995b72fddcef9a
Roger <rdpender3@gmail.com>
2018-07-05 19:08:22 -0700

    adding javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,92 @@
+// need victory song
+// need wrong answer song
+// audio array
+// need to set time limit for game and time limit for new question to appear
+// 
+
+function resetQuestions () {
+    return {
+        "Question1": {
+            Question: "Who lived in the pit for a good amount of time?",
+            Wrong1: "Ann Perkins",
+            Wrong2: "Ron Swanson",
+            Right: "Andy Dwyer",
+            Wrong3: "Jerry"
+        },
+
+        "Question2": {
+            Question: "Ann's original profession was: "
+            Wrong1: "Public relations",
+            Right: "Nursing",
+            Wrong2: "Teacher",
+            Wrong3: "Waitress"
+        },
+
+        "Question3": {
+            Question: "What state is Pawnee located in?"
+            Wrong1: "Utah",
+            Wrong2: "Missouri",
+            Wrong3: "Montana",
+            Right: "Indiana"
+        },
+
+        "Question4": {
+            Question: "What was the first company that Tom helped create?"
+            Right: "Entertainment 720",
+            Wrong1: "Tom's Bistro",
+            Wrong2: "Snake Hole Lounge",
+            Wrong3: "Rent-A-Swag"
+        },
+
+        "Question5": {
+            Question: "Which of these names was never a possibile Mouse Rat name?"
+            Wrong1: "Two Doors Down",
+            Right: "Just the Tiptoe",
+            Wrong2: "Scarecrow Boat",
+            Wrong3: "Fiveskin"
+        },
+
+        "Question6": {
+            Question: "Where do Ann and Chris move to at the end of the series?"
+            Wrong1: "Florida",
+            Wrong2: "Illinois",
+            Right: "Michigan",
+            Wrong3: "New York"
+        },
+
+        "Question7": {
+            Question: "Why did Tom and Wendy's marriage end?"
+            Wrong1: "They weren't in love anymore",
+            Right: "It was a green card marriage",
+            Wrong2: "Wendy had to move back to Canada",
+            Wrong3: "Tom cheated on Wendy"
+        },
+
+        "Question8": {
+            Question: "How old was Ben when the Ice Town incident occured"
+            Wrong1:
+            Wrong2:
+            Wrong3:
+            Right:
+        },
+
+        "Question9": {
+            Question:
+            Right:
+            Wrong1:
+            Wrong2:
+            Wrong3:
+        },
+
+        "Question10": {
+            Question:
+            Wrong1:
+            Wrong2:
+            Right:
+            Wrong3:
+        },
+    }
+}
 // figure out a theme (parks and rec trivia? game of thrones trivia? arrested development? marvel studios trivia?)
 // need to make arrays of questions with right and wrong answers
 // link a gif to them
