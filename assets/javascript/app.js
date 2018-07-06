0940a615e6c8a60a1bda0a312072c68a3e33e1e7
Roger <rdpender3@gmail.com>
2018-07-05 23:26:17 -0700

    fixed more arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,77 +43,10 @@ function gameStart () {
 
 var questionArray = ["Who lived in the pit for a good amount of time?", "Ann's original profession was: ", "What state is Pawnee located in?", "What was the first company that Tom helped create?", "What is the name of Pawnee's beloved mini horse?", "What is Jerry's real name?", "How old was Ben when the Ice Town incident occured", "Which character was only supposed to be in Season 1?", "What board game did Ben create?", "What is Donna and Tom's signature catchphrase?"];
 
-var answerArray = []
-        "Question1": {
-            Question: 
-            Option: ["Ann Perkins", "Ron Swanson", "Andy Dwyer",  "Jerry"],
-            Answer: 0,
-            image: "assets/images/andy.gif"
-        },
+var answerArray = [["Ann Perkins", "Ron Swanson", "Andy Dwyer",  "Jerry"], ["Public relations", "Nursing", "Teacher", "Waitress"], ["Utah", "Missouri", "Montana", "Indiana"], ["Entertainment 720", "Tom's Bistro", "Snake Hole Lounge", "Rent-A-Swag"], ["Li'l Agnes", "Li'l Sebastian", "Li'l Tom", "Li'l Fred"], ["Terry", "Barry", "Garry", "Larry"], ["24", "16", "21", "18"], ["Andy Dwyer", "Tom Haverford", "April Ludgate", "Ron Swanson"], ["The Iron Throne", "You're an Accountant Harry Potter", "The Cones of Dunshire", "Count Chocula versus Frankenberry"], ["It's Wednesday my dudes", "All black everythang", "What happens in Pawnee ends up on social media", "Treat yo self"]];
 
-        "Question2": {
-            Question: 
-            Option: ["Public relations", "Nursing", "Teacher", "Waitress"],
-            Right: 1,
-            image: "assets/images/ann.gif"
-        },
-
-        "Question3": {
-            Question: 
-            Option: ["Utah", "Missouri", "Montana", "Indiana"],
-            Right: 3,
-            image: "assets/images/pawnee-indiana.gif"
-        },
-
-        "Question4": {
-            Question: 
-            Option: ["Entertainment 720", "Tom's Bistro", "Snake Hole Lounge", "Rent-A-Swag"],
-            Right: 0,
-            image: "assets/images/tom.gif"
-        },
-
-        "Question5": {
-            Question: 
-            Option1: ["Li'l Agnes", "Li'l Sebastian", "Li'l Tom", "Li'l Fred"],
-            Right: 1,
-            image: "assets/images/sebastian.gif"
-        },
-
-        "Question6": {
-            Question: 
-            Option: ["Terry", "Barry", "Garry", "Larry"],
-            Answer: 2,
-            image: "assets/images/garry.gif"
-        },
-
-        "Question7": {
-            Question: 
-            Option: ["24", "16", "21", "18"],
-            Right: 3,
-            image: "assets/images/ben.gif"
-        },
-
-        "Question8": {
-            Question: 
-            Option: ["Andy Dwyer", "Tom Haverford", "April Ludgate", "Ron Swanson"],
-            Right: 0,
-            image: "assets/images/andy-crying.gif"
-        },
-
-        "Question9": {
-            Question: 
-            Option: ["The Iron Throne", "You're an Accountant Harry Potter", "The Cones of Dunshire", "Count Chocula versus Frankenberry"],
-            Right: 2,
-            image: "assets/images/cones.gif"
-        },
-        
-        "Question10": {
-            Question: ,
-            Option: ["It's Wednesday my dudes", "All black everythang", "What happens in Pawnee ends up on social media", "Treat yo self"],
-            Right: 3,
-            image: "assets/images/treat-yo-self.gif"
-        }
-    ];
+var gifArray = ["assets/images/andy.gif", "assets/images/ann.gif", "assets/images/pawnee-indiana.gif", "assets/images/tom.gif", "assets/images/sebastian.gif", "assets/images/garry.gif", "assets/images/ben.gif", "assets/images/andy-crying.gif", "assets/images/cones.gif", "assets/images/treat-yo-self.gif"]
+      
 
 // function createQuestion() {
 //     var questionDiv = $("<div class='questions' data-name>" + key + "</div>");
