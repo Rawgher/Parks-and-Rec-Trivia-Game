afeef911bce15f2773d4c86e2b90c5dc32dafadc
Roger <rdpender3@gmail.com>
2018-07-07 11:03:40 -0700

    final version - maybe

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,3 @@
-// need victory song
-// need wrong answer song
-// audio array
-// need to set time limit for game and time limit for new question to appear
-// 
-
-// set answer options to an array and then set the write anser on an array
-// questions = ""
-// answer = [array of answers]
-// answer = string number of what is the answer
-
 var startGame;
 var counter = 20;
 var rightAnswers = 0;
@@ -18,7 +7,8 @@ var questionNumber = 0;
 var correctOption;
 var gameTimer;
 var gameText;
-var audio;
+var audio = "assets/audio/theme-song.m4a";
+
 var questionArray = ["Who lived in the pit for a good amount of time?", "Ann's original profession was: ", "What state is Pawnee located in?", "What was the first company that Tom helped create?", "What is the name of Pawnee's beloved mini horse?", "What is Jerry's real name?", "How old was Ben when the Ice Town incident occured", "Which character was only supposed to be in Season 1?", "What board game did Ben create?", "What is Donna and Tom's signature catchphrase?"];
 
 var answerArray = [["Ann Perkins", "Ron Swanson", "Andy Dwyer",  "Jerry"], ["Public relations", "Nursing", "Teacher", "Waitress"], ["Utah", "Missouri", "Montana", "Indiana"], ["Entertainment 720", "Tom's Bistro", "Snake Hole Lounge", "Rent-A-Swag"], ["Li'l Agnes", "Li'l Sebastian", "Li'l Tom", "Li'l Fred"], ["Terry", "Barry", "Garry", "Larry"], ["24", "16", "21", "18"], ["Andy Dwyer", "Tom Haverford", "April Ludgate", "Ron Swanson"], ["The Iron Throne", "You're an Accountant Harry Potter", "The Cones of Dunshire", "Count Chocula versus Frankenberry"], ["It's Wednesday my dudes", "All black everythang", "What happens in Pawnee ends up on social media", "Treat yo self"]];
@@ -27,29 +17,20 @@ var correctAnswers = ["C. Andy Dwyer", "B. Nursing", "D. Indiana", "A. Entertain
 
 var gifArray = ["assets/images/andy.gif", "assets/images/ann.gif", "assets/images/pawnee-indiana.gif", "assets/images/tom.gif", "assets/images/sebastian.gif", "assets/images/garry.gif", "assets/images/ben.gif", "assets/images/andy-crying.gif", "assets/images/cones.gif", "assets/images/treat-yo-self.gif"];
 
-var audioArray = ["assets/audio/theme-song.m4a", "assets/audio/sebastian.m4a", "assets/audio/johnny-karate.m4a"];
-
-function randomAudioPick() {
-    return this.audioArray[Math.floor(Math.random() * this.audioArray.length)];
-};
-
-$(document).ready(function() {
-
-    
+$(document).ready(function() { 
 
 function gameStart () {
-    startGame ="<div class='flex items-center justify-center pa4'><a class='f6 grow no-underline ph3 pv2 mb2 dib white bg-dark-blue start' href='#' role='button'>Start the quiz</a></div>";
-	$("#quizContainer").html(startGame);
+    startGame ="<div class='flex items-center justify-center pa2'><h1 style='font-size: 60px;'>Parks and Rec Trivia</h1></div><div class='flex items-center justify-center pa2'><a class='f6 grow no-underline ph3 pv2 mb2 dib white bg-dark-blue start' href='#' role='button'>Start the quiz</a></div>";
+    $("#quizContainer").html(startGame);
 }
 
 gameStart();
 
 $("#quizContainer").on("click", ".start", function() {
-var audio = document.createElement("audio");
-    audio.setAttribute('src', randomAudioPick());
-    audioArray.loop = true;
-    audio.play();
-    
+    var song = document.createElement("audio");
+    song.setAttribute('src', audio);
+    song.loop = true;
+    song.play();
 
 	generateQuestions();
 
@@ -72,8 +53,10 @@ var audio = document.createElement("audio");
     });
 
     $("#quizContainer").on("click", ".reset", function(){
-        
         resetGame();
+        console.log("audio function here")
+        stopAudio();
+         
     });
 
 });
@@ -133,10 +116,6 @@ function timer() {
 	}
 }
 
-
-
-
-
 function gameOver() {
 	gameText = "<div class='text-center flex items-center justify-center pa2'>That's it! Let's see how you did!" + "</div>" + "<div class='rightAnswers flex items-center justify-center pa2'>Correct Answers: " + rightAnswers + "</div>" + "<div class='flex items-center justify-center pa2'>Wrong Answers: " + wrongAnswers + "</div>" + "<div class='flex items-center justify-center pa2'>Unanswered: " + unanswered + "</div>" + "<div class='text-center reset-button-container flex items-center justify-center pa2'><a class='f6 grow no-underline ph3 pv2 mb2 dib white bg-dark-blue reset' href='#' role='button'>Play again!</a></div>";
     $("#quizContainer").html(gameText);
@@ -148,11 +127,6 @@ function resetGame() {
 	wrongAnswers = 0;
 	unanswered = 0;
 	counter = 20;
-	generateQuestions();
+    generateQuestions();
     timer();
-    playAudio();
-
 }
-
-// need to fix div so things disappear after answered
-// need reset to make everything disappear and then start over
